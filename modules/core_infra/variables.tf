98b42f0e58c3f55aa94330b06c785729a06bd524
Major Mason <sasukefan772@gmail.com>
2025-09-24 22:12:18 -0400

    refactored virtual_machine.tf's code

--- modules/core_infra/variables.tf
+++ modules/core_infra/variables.tf
@@ -25,3 +25,18 @@ variable "storage_account_type" {
   description = "Storage account SKU to be lowest tier"
   type        = string
 }
+
+variable "vm_size" {
+  description = "Virtual Machine size"
+  type = string
+}
+
+variable "eviction_policy" {
+  description = "The action performed if the spot price exceeds the maximum bid price"
+  type = string
+}
+
+variable "max_bid_price" {
+  description = "The maximum price you're willing to pay (in USD) for the VM's hourly rate"
+  type = number
+}
--- modules/core_infra/virtual.machine.tf
+++ modules/core_infra/virtual.machine.tf
@@ -1,15 +1,15 @@
 #A simple Linux VM on the cheapest available size from Azure offerings
 #Note: Max Bid Price is the maximum price you're willing to pay for this VM (in USD) and if the bid
 #price falls below the current spot price, the VM will be evicted automatically
-resource "azurerm_linux_virtual_machine" "dev-vm" {
-  name                  = "dev-vm"
-  resource_group_name   = azurerm_resource_group.dev-rg.name
-  location              = azurerm_resource_group.dev-rg.location
-  size                  = "B2ats v2"
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "${var.environment}-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = var.vm_size
   admin_username        = "adminuser"
   network_interface_ids = [azurerm_network_interface.dev-nic.id]
-  eviction_policy       = "Deallocate"
-  max_bid_price         = 0.02
+  eviction_policy       = var.eviction_policy
+  max_bid_price         = var.max_bid_price
 
   #Note that the file "customdata.tpl" is a template file indicated by the file extension "tpl"
   #Template files are very nice to pass in a series of commands for things like VMs
