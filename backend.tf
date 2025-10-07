1e3f399cdb1b7836528a6c6285cee3f2cf4a5439
Major Mason <sasukefan772@gmail.com>
2025-10-06 21:51:17 -0400

    adding in necessary strings and booleans to complete the backend codeblock for allowing TF to store state files in storage account

--- backend.tf
+++ backend.tf
@@ -1,10 +1,19 @@
 #This file allows us to store our Terraform state file in a secure location, namely
 #an Azure storage account, as denoted below
+#NOTE: Required information includes storage account name, container name, and key
 terraform {
   backend "azurerm" {
-    resource_group_name  = "${var.environment}-rg"
-    storage_account_name = "statestorage"
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    tenant_id                        = "000000"
+    client_id                        = "000000"
+    oidc_azure_service_connection_id = "000000"
+    use_oidc                         = true
+    use_azuread_auth                 = true
+    storage_account_name             = "statestorage"
+    container_name                   = "tfstate"
+    key                              = "dev.terraform.tfstate"
   }
 }
+#The "client_id" is the client ID value of the SP, App Reg, or MI set aside for Terraform to use
+#The "oidc_azure_service_connection_id" value is the service connection value from the settings
+#page within ADO itself, and is generated automatically in ADO when you set that up
+#The "key" is just the name of the blob file that we'll write the TF state info into
