ae2928088e330b5552dd3d1429e9ab0911d127ff
Major Mason <sasukefan772@gmail.com>
2025-11-12 18:25:24 -0500

    further modifications to our backend.tf file to accommodate for dev and prod storage accounts under the same subscription

--- backend.tf
+++ backend.tf
@@ -2,18 +2,31 @@
 #an Azure storage account, as denoted below
 #NOTE: Required information includes storage account name, container name, and key
 terraform {
-  backend "azurerm" {
+  backend "dev_storage" {
     tenant_id                        = "000000"
     client_id                        = "000000"
     oidc_azure_service_connection_id = "000000"
     use_oidc                         = true
     use_azuread_auth                 = true
-    storage_account_name             = "${var.environment}-storage-account"
-    container_name                   = "${var.environment}-storage-container"
-    key                              = "${var.environment}.tfstate"
+    storage_account_name             = "dev-storage-account"
+    container_name                   = "dev-storage-container"
+    key                              = "dev.tfstate"
   }
 }
 #The "client_id" is the client ID value of the SP, App Reg, or MI set aside for Terraform to use
 #The "oidc_azure_service_connection_id" value is the service connection value from the settings
 #page within ADO itself, and is generated automatically in ADO when you set that up
 #The "key" is just the name of the blob file that we'll write the TF state info into
+
+terraform {
+  backend "prod_storage" {
+    tenant_id                        = "000000"
+    client_id                        = "000000"
+    oidc_azure_service_connection_id = "000000"
+    use_oidc                         = true
+    use_azuread_auth                 = true
+    storage_account_name             = "prod-storage-account"
+    container_name                   = "prod-storage-container"
+    key                              = "prod.tfstate"
+  }
+}
