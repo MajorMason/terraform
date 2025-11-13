a3725bbbf1bc35a41f23defa7e1f526f13dda89f
Major Mason <sasukefan772@gmail.com>
2025-11-13 17:51:05 -0500

    added a learning material file to encapsulate our understanding of state vs tfvars and how to store them/access them

--- backend.tf
+++ backend.tf
@@ -7,12 +7,10 @@ terraform {
     client_id            = "000000"
     use_oidc             = true
     use_azuread_auth     = true
-    storage_account_name = "tfstate-storage"
+    storage_account_name = "terraform-storage"
     container_name       = "tfstate"
     key                  = "dev.tfstate"
   }
 }
 #The "client_id" is the client ID value of the SP, App Reg, or MI set aside for Terraform to use
-#The "oidc_azure_service_connection_id" value is the service connection value from the settings
-#page within ADO itself, and is generated automatically in ADO when you set that up
 #The "key" is just the name of the blob file that we'll write the TF state info into
