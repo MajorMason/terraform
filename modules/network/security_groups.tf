1a03a21eb460ea92a5d08327c5b800e8ac4aec75
Major Mason <sasukefan772@gmail.com>
2026-02-23 00:50:21 -0500

    deleted old NSR and replaced it with two new ones for frontend containers to allow traffic

--- modules/network/security_groups.tf
+++ modules/network/security_groups.tf
@@ -1,24 +1,45 @@
 #Azure NSGs are stateful, and collapse both the ACLs and Security Groups into one
-#Unlike AWS that has separate methods (used together) for ACLs and Security Groups,
+#Unlike AWS that has separate methods (used together) via ACLs and Security Groups,
 #Azure only needs just NSGs and their rules (stateful only) to secure everything
-resource "azurerm_network_security_group" "backend_nsg" {
-  name = "backend-nsg"
+resource "azurerm_network_security_group" "frontend_nsg" {
+  name = "${var.environment}-frontend-nsg"
   location = var.location
   resource_group_name = var.resource_group_name
 }
 
-#The [0] at the end of the source and destination prefix strings simply tell Terraform
-#to only supply the first item in said lists which is the network address itself
-resource "azurerm_network_security_rule" "fe-be" {
-  name = "${var.environment}-febe-internal-allow"
+resource "azurerm_network_security_group" "backend_nsg" {
+  name = "${var.environment}-backend-nsg"
+  location = var.location
   resource_group_name = var.resource_group_name
-  priority = 100
-  direction = "Inbound"
-  access = "Allow"
-  protocol = "Tcp"
-  source_port_range = "*"
-  destination_port_range = "8080"
-  source_address_prefix = var.public_address_prefix[0]
-  destination_address_prefix = var.private_address_prefix[0]
-  network_security_group_name = azurerm_network_security_group.backend_nsg
+}
+
+#Since NSGs are stateful, you create inbound allow rules on the NSG attached to either:
+#-the subnet hosting your container environment, or
+#-the NIC of the compute resource (VM, Container Instance, etc.)
+resource "azurerm_network_security_rule" "fe_80_nsr" {
+  name                        = "fe-80-allow"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
+
+resource "azurerm_network_security_rule" "fe_443_nsr" {
+  name                        = "fe-443-allow"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.backend_nsg.name
 }
